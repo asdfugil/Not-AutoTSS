ef5d8a3c9251850bcf23e527d37fe3d22138ebed
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-08-31 04:35:53 +0000

    database

--- commands/devices.js
+++ commands/devices.js
@@ -1,6 +1,8 @@
 const { CommandInteraction, Interaction } = require('discord.js')
 const { sequelize, device } = require('../lib/models.js')
+const MAX_DEVICES = parseInt(process.env.MAX_DEVICES)
 const fs = require('fs')
+const fetch = require('node-fetch')
 const { save } = require('../lib/tss.js')
 module.exports = {
     name: 'devices',
@@ -11,23 +13,46 @@ module.exports = {
              * @param { CommandInteraction } interaction 
              */
             async execute(interaction) {
-                //const owned_devices = await device.findAll({ where: { owner: BigInt(interaction.user.id) } })
-                const ecid = BigInt(interaction.options.getString('ecid'))
-                //const existing_device = await device.findByPk(ecid, { raw: true })
-                //if (existing_device) {
-                //    const owner = await interaction.client.users.fetch(existing_device.owner)
-                //    interaction.reply(`The device with this ECID is already registered under the Discord user ${owner.tag}!`)
-                //}
-
-                const { ticket, version, build_no } = save({
-                    model: interaction.options.getString('model'),
-                    ecid,
-                    generator: BigInt(interaction.options.getString('generator', false)),
-                    apnonce: interaction.options.getString('apnonce', false),
-                    boardconfig: interaction.options.getString('boardconfig')
+                const owned_devices = await device.findAll({ where: { owner: interaction.user.id }, raw: true })
+                if (owned_devices.length >= MAX_DEVICES) interaction.reply(`You have maxed out the number of your devices`)
+                const ecid = interaction.options.getString('ecid')
+                const existing_device = await device.findByPk(ecid, { raw: true })
+                if (existing_device && existing_device.owner !== interaction.user.id) {
+                    const owner = await interaction.client.users.fetch(existing_device.owner)
+                    interaction.reply(`The device with this ECID is already registered under the Discord user ${owner.tag}!`)
+                    return
+                } else if (existing_device) {
+                    interaction.reply('You already added this device.')
+                    return
+                }
+                interaction.deferReply()
+                const generator = interaction.options.getString('generator', false) || null
+                const apnonce = interaction.options.getString('apnonce', false) || null
+                const model = interaction.options.getString('model')
+                const boardconfig = interaction.options.getString('boardconfig')
+                await device.upsert({
+                    model, ecid, generator, apnonce, boardconfig,
+                    owner: interaction.user.id
                 })
-                if (!fs.existsSync('./blobs/' + ecid.toString(16))) fs.mkdirSync('./blobs/' + ecid.toString(16))
-                fs.writeFileSync(`./blobs/${interaction.options.getString('model')}_${ecid.toString(16)}_${version}_${build_no}.shsh2`,ticket)
+                let generators;
+                if (!generator)
+                    generators = ['0xbd34a880be0b53f3', '0x1111111111111111']
+                else
+                    generators = [`${generator.toString(16)}`]
+                const signed_fw = await fetch('https://api.m1sta.xyz/betas/' + model, {
+                    headers: { 'user-agent': 'Not-AutoTSS/0' }
+                })
+                    .then(response => response.json())
+                    .then(json => json.filter(x => x.signed))
+                for (gen of generators) {
+                    const blobs = await Promise.all(signed_fw.map(fw => save({ model, ecid, generator:gen, apnonce, boardconfig, buildid: fw.buildid, version: fw.version })))
+                    for (const blob in blobs) {
+                        const blob_save_path = `./blobs/${ecid.toString(16)}/${blob.version}/${blob.buildid}${apnonce ? `/apnonce-${apnonce}` : ''}`
+                        if (!fs.existsSync(blob_save_path)) fs.mkdirSync(blob_save_path)
+                        fs.writeFileSync(`${blob_save_path}/${gen.toString(16)}_${ecid.toString(16)}.shsh2`)
+                        console.log(`Saved blob`)
+                    }
+                }
                 interaction.reply('Device added.')
             }
         }
